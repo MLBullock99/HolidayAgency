ff5f73bfdde7545401d92541dad0f4abe184a988
MLBullock99 <mlbullock99@gmail.com>
2025-07-27 16:02:00 +0100

    add vehicle comparison check

--- src/main/java/holidayagency/vehicle/GetCheapestVehicleUseCase.java
+++ src/main/java/holidayagency/vehicle/GetCheapestVehicleUseCase.java
@@ -1,10 +1,18 @@
 package holidayagency.vehicle;
 
+
 import holidayagency.vehicle.models.CheapestVehicleModel;
+
+import static holidayagency.vehicle.VehicleConstants.*;
 
 class GetCheapestVehicleUseCase {
 
     public CheapestVehicleModel getCheapestVehicle(int distance) {
-        return null;
+        int taxiCost = distance * TAXI_COST_PER_MILE;
+        int carCost = distance * CAR_COST_PER_MILE + CAR_PARKING_COST;
+
+        return taxiCost < carCost
+                ? new CheapestVehicleModel("Taxi", taxiCost)
+                : new CheapestVehicleModel("Car", carCost);
     }
 }
---
+++ src/main/java/holidayagency/vehicle/VehicleConstants.java
@@ -0,0 +1,9 @@
+package holidayagency.vehicle;
+
+final class VehicleConstants {
+
+    public static final int TAXI_COST_PER_MILE = 40;
+    public static final int CAR_COST_PER_MILE = 20;
+    public static final int CAR_PARKING_COST = 300;
+
+}
